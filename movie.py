9548974ee6cd8ed8c914bf9272a4d059ab5d0060
Adrian Cowan (othrayte) <othrayte@gmail.com>
2012-02-18 23:55:19 +1100

    Added first peice of comparision logic

--- movie.py
+++ movie.py
@@ -145,6 +145,9 @@ class Movie(SQLObject):
     return out
     
     #Merging objects
+    def merge(self, other):
+        self |= other
+
     def __ior__(self, other):
         for key in other.keys():
             if key in self:
@@ -181,32 +184,63 @@ class Movie(SQLObject):
         other.destroySelf()
 
     #Enumerate diff
-    def __xor__(self, other):
-        for key in self.keys():
-            if self[key] <> other
+    def diff(self, left, right):
+        return self ^ (left, right)
 
-# if nothing to do (externals unknown or all same)
-    #next
-# if new data (cache unknown)
-    # if soft
-        #next
-    # else
-        # if l unknown
-# if simple (not all diff)
-# All diff
+    def __xor__(self, others):
+        left = other[0]
+        right = other[1]
+        changes = []
+        for key in self.keys():
+            cacheV = self[key]
+            cacheN = cacheV is Null
+            leftN = left[key] is Null
+            rightN = right[key] is Null
+            soft = key in _bestBefore
+            leftD = left[key] <> cacheV
+            rightD = right[key] <> cacheV
+            sidesD = left[key] <> right[key]
+            if cacheN: # if cache null
+                if not leftN and not rightN: # if not all null
+                    if leftN: # if left null
+                        changes.append({'dest': 'cache', 'attr': key, 'value': right[key], 'soft': true})# <~
+                    elif rightN:# if right null
+                        changes.append({'dest': 'cache', 'attr': key, 'value': left[key], 'soft': true})# >~
+                    else:
+                        if sidesD:# if sides differ
+                            changes.append(best({'dest': 'cache', 'attr': key, 'value': left[key], 'soft': false},{'dest': 'cache', 'attr': key, 'value': right[key], 'soft': false}))# ? fight
+                        else:# else
+                            changes.append({'dest': 'cache', 'attr': key, 'value': left[key], 'soft': false})# ><
+            elif not (leftN or cacheN or rightN) and (not (leftD or rightD or sidesD or soft) or ((leftD <> rightD) and soft))# elif all not null and (all same and not soft) or (only 1 side same and soft)
+                changes.append(best({'dest': 'cache', 'attr': key, 'value': left[key], 'soft': false},{'dest': 'cache', 'attr': key, 'value': right[key], 'soft': false}))# ? fight
+            elif leftD and not leftN:# elif left differs and isn't null
+                if rightN and soft:# if right null and cache soft
+                    changes.append({'dest': 'cache', 'attr': key, 'value': left[key], 'soft': true})# >~
+                elif rightN or rightD:# elif right null or right differs
+                    changes.append({'dest': 'right', 'attr': key, 'value': left[key], 'soft': false})# >
+                else:# else (both sides had same change)
+                    changes.append({'dest': 'cache', 'attr': key, 'value': left[key], 'soft': false})# ><
+            elif rightD and not rightN:# elif right differs and isn't null
+                if soft:# if soft
+                    changes.append({'dest': 'cache', 'attr': key, 'value': right[key], 'soft': true})# <~
+                else:# else
+                    changes.append({'dest': 'left', 'attr': key, 'value': right[key], 'soft': false})# <
+            # else
+                # do nothing
+
 """
 Three way compare logic
-    /  ~
-??? -  -
-??1 <~ -
-?1? -  -
-?11 <  -
-?12 <  <~
-1?? >~ -
-1?1 >< -
-1?2 ?  -
-11? >  -
-111 -  -
+LCR /  ~  (soft = false /, true ~)
+??? -  -  (L = left, C = cache, R = right)
+??1 <~ -  (? input (L or R) = unknown)
+?1? -  -  (? input C = cache empty)
+?11 <  -  (? result = compare and decide)
+?12 <  <~ (- = do nothing)
+1?? >~ -  (< = send R to L then C)
+1?1 >< -  (> = send L to R then C)
+1?2 ?  -  (<~ = send R to C, leave soft)
+11? >  -  (>~ = send L to C, leave soft)
+111 -  -  (>< = send L or R to C (L & R must have been the same))
 112 <  ?
 12? >  >~
 121 >< ><
